57e62e714bf972b742659b12466a5110bdc29fcf
Oscar Jimenez <fox.oje11@gmail.com>
2018-10-25 23:56:49 -0600

    Avance con directivas de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { VideoJuegoComponent } from './videojuego/videojuego.component';
@@ -14,7 +15,8 @@ import { CursoComponent } from './curso/curso.component';
     CursoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -10,17 +10,58 @@ import { Zapatilla } from '../models/zapatilla';
 export class ZapatillasComponent implements OnInit {
 	public titulo: string = 'Componente de Zapatillas';
 	public zapatillas: Array<Zapatilla>;
+	public marcas: String[];
+	public color: string;
+	public mi_marca: string;
 
-	constructor() {
+	constructor(){
+		this.mi_marca = 'Fox';
+		this.color = 'blue';
+		this.marcas = new Array;
 		this.zapatillas = [
 			new Zapatilla('Nike Airmax', 'Nike', 'Rojo', 190, true),
-			new Zapatilla('Reebok Classic', 'Reebok', 'Blanco', 80, true),
+			new Zapatilla('Reebok Classic', 'Reebook', 'Blanco', 80, true),
+			new Zapatilla('Reebook Spartan', 'Reebook', 'Negro', 175, false),
 			new Zapatilla('Nike Runner MD', 'Nike', 'Negro', 60, true),
-			new Zapatilla('Adidas Yezzy', 'Adidas', 'Gris', 180, false)
+			new Zapatilla('Adidas Yezzy', 'Adidas', 'Gris', 180, false),
 		];
 	}
 
-	ngOnInit() {
+	public ngOnInit(){
 		console.log(this.zapatillas);
+		this.getMarcas();
+	}
+
+	public getMarcas(){
+		this.zapatillas.forEach((zapatilla, index) => {
+			if (this.marcas.indexOf(zapatilla.marca) < 0)
+				this.marcas.push(zapatilla.marca);
+		});
+
+		console.log(this.marcas);
+	}
+
+	public getMarca(){
+		alert(this.mi_marca);
+	}
+
+	public addMarca(){
+		if (this.marcas.indexOf(this.mi_marca) < 0)
+			this.marcas.push(this.mi_marca);
+		else
+			alert('La marca ' + this.mi_marca + ' ya pertenece a la lista!');
+	}
+
+	public borrarMarca(index){
+		//delete this.marcas[index];
+		this.marcas.splice(index, 1);
+	}
+
+	public onBlur(){
+		console.log('Has salido del input');
+	}
+
+	public mostrarPalabra(){
+		alert(this.mi_marca);
 	}
 }
